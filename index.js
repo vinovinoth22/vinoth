901085b0a4a8239a69f58510eaa8f3d14d1c921d
unknown <vinothvino40083@gmail.com>
2023-12-23 12:08:08 +0530

    message

---
+++ index.js
@@ -0,0 +1,9 @@
+
+    //console.log(20+20)
+     //document.write("hell")
+     //document.write("hello")
+     //document.writeln("book pen")
+     //document.writeln("for my boy")
+     //document.write("@aliasceasar")
+     alert("bloody sweet!!!!")
+     
